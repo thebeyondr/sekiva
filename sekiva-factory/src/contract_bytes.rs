ee31a26c7ad745e7a7619969f7da31a14f369420
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2025-04-29 13:10:09 -0500

    feat: :sparkles: refactor SekivaFactory to remove contract bytes and enhance initialization with direct wasm and abi handling

--- sekiva-factory/src/contract_bytes.rs
+++
@@ -1,9 +0,0 @@
-pub const ORGANIZATION_WASM: [u8; 82378] =
-    *include_bytes!("../../target/wasm32-unknown-unknown/release/organization.wasm");
-pub const ORGANIZATION_ABI: [u8; 512] =
-    *include_bytes!("../../target/wasm32-unknown-unknown/release/organization.abi");
-
-pub const BALLOT_WASM: &[u8; 91859] =
-    include_bytes!("../../target/wasm32-unknown-unknown/release/ballot.wasm");
-pub const BALLOT_ABI: &[u8; 641] =
-    include_bytes!("../../target/wasm32-unknown-unknown/release/ballot.abi");
--- sekiva-factory/src/lib.rs
+++ sekiva-factory/src/lib.rs
@@ -6,15 +6,25 @@ extern crate pbc_contract_codegen;
 extern crate pbc_contract_common;
 
 use create_type_spec_derive::CreateTypeSpec;
-use pbc_contract_common::address::Address;
+use pbc_contract_common::address::{Address, AddressType, Shortname, ShortnameCallback};
 use pbc_contract_common::context::{CallbackContext, ContractContext};
 use pbc_contract_common::events::EventGroup;
 use pbc_contract_common::sorted_vec_map::{SortedVecMap, SortedVecSet};
-use pbc_traits::ReadWriteState;
+use pbc_traits::{ReadWriteState, WriteRPC};
 use read_write_rpc_derive::ReadWriteRPC;
 use read_write_state_derive::ReadWriteState;
 
-mod contract_bytes;
+const DEPLOY_CONTRACT_ADDRESS: Address = Address {
+    address_type: AddressType::SystemContract,
+    identifier: [
+        0x97, 0xa0, 0xe2, 0x38, 0xe9, 0x24, 0x02, 0x5b, 0xad, 0x14, 0x4a, 0xa0, 0xc4, 0x91, 0x3e,
+        0x46, 0x30, 0x8f, 0x9a, 0x4d,
+    ],
+};
+
+const DEPLOY_SHORTNAME: Shortname = Shortname::from_u32(4);
+const BINDER_ID: u32 = 9;
+const DEPLOY_ORGANIZATION_CALLBACK: ShortnameCallback = ShortnameCallback::from_u32(0x10);
 
 #[derive(CreateTypeSpec, ReadWriteState, Clone)]
 #[repr(u8)]
@@ -39,6 +49,8 @@ struct BallotInfo {
     status: BallotStatus,
     created_at: u64,
     administrator: Address,
+    description: String,
+    options: Vec<String>,
 }
 
 #[derive(CreateTypeSpec, ReadWriteState, Clone)]
@@ -59,32 +71,52 @@ struct OrganizationInfo {
 
 #[state]
 pub struct SekivaFactoryState {
+    admin: Address,
     organizations: SortedVecSet<Address>,
-    ballots: SortedVecMap<Address, BallotInfo>,
+    ballots: SortedVecSet<Address>,
     user_organizations: SortedVecMap<Address, SortedVecSet<Address>>,
     user_ballots: SortedVecMap<Address, SortedVecSet<Address>>,
     deployed_contracts: SortedVecMap<Address, ContractType>,
+    ballot_contract_zkwa: Vec<u8>,
+    ballot_contract_abi: Vec<u8>,
+    organization_contract_wasm: Vec<u8>,
+    organization_contract_abi: Vec<u8>,
 }
 
-/// Initialize a new Sekiva Factory.
+/// Initial function to create the initial state.
 ///
-/// # Arguments
+/// ### Parameters:
 ///
-/// * `_ctx` - the contract context containing information about the sender and the blockchain.
+/// * `ctx`: [`ContractContext`], initial context.
+/// * `ballot_contract_zkwa`: [`Vec<u8>`], wasm bytes of a ballot contract.
+/// * `ballot_contract_abi`: [`Vec<u8>`], abi bytes of a ballot contract.
+/// * `organization_contract_wasm`: [`Vec<u8>`], wasm bytes of an organization contract.
+/// * `organization_contract_abi`: [`Vec<u8>`], abi bytes of an organization contract.
 ///
-/// # Returns
-///
-/// The initial state of the Sekiva Factory.
-///
+/// ### Returns:
+/// The initial state of type [`SekivaFactoryState`].
 #[init]
-pub fn initialize(_ctx: ContractContext) -> SekivaFactoryState {
-    SekivaFactoryState {
+pub fn initialize(
+    ctx: ContractContext,
+    ballot_contract_zkwa: Vec<u8>,
+    ballot_contract_abi: Vec<u8>,
+    organization_contract_wasm: Vec<u8>,
+    organization_contract_abi: Vec<u8>,
+) -> (SekivaFactoryState, Vec<EventGroup>) {
+    let state = SekivaFactoryState {
+        admin: ctx.sender,
         organizations: SortedVecSet::new(),
-        ballots: SortedVecMap::new(),
+        ballots: SortedVecSet::new(),
         user_organizations: SortedVecMap::new(),
         user_ballots: SortedVecMap::new(),
         deployed_contracts: SortedVecMap::new(),
-    }
+        ballot_contract_zkwa,
+        ballot_contract_abi,
+        organization_contract_wasm,
+        organization_contract_abi,
+    };
+
+    (state, vec![])
 }
 
 /// Create a new organization.
@@ -93,6 +125,10 @@ pub fn initialize(_ctx: ContractContext) -> SekivaFactoryState {
 ///
 /// * `ctx` - the contract context containing information about the sender and the blockchain.
 /// * `state` - the current state of the Sekiva Factory.
+/// * `name` - the name of the organization.
+/// * `description` - the description of the organization.
+/// * `profile_image` - the profile image of the organization.
+/// * `banner_image` - the banner image of the organization.
 ///
 /// # Returns
 ///
@@ -107,15 +143,14 @@ fn deploy_organization(
     profile_image: String,
     banner_image: String,
 ) -> (SekivaFactoryState, Vec<EventGroup>) {
-    // Prepare deployment
     let mut event_group = EventGroup::builder();
 
-    // Call system deploy contract
     event_group
         .call(DEPLOY_CONTRACT_ADDRESS, DEPLOY_SHORTNAME)
-        .argument(contract_bytes::ORGANIZATION_WASM)
-        .argument(contract_bytes::ORGANIZATION_ABI)
-        .argument(create_init_data(
+        .argument(BINDER_ID)
+        .argument(state.organization_contract_wasm.clone())
+        .argument(state.organization_contract_abi.clone())
+        .argument(create_org_init_data(
             name,
             description,
             profile_image,
@@ -123,7 +158,6 @@ fn deploy_organization(
         ))
         .done();
 
-    // Add callback to track newly deployed contract
     event_group
         .with_callback(SHORTNAME_DEPLOY_ORGANIZATION_CALLBACK)
         .with_cost(10000)
@@ -132,7 +166,17 @@ fn deploy_organization(
     (state, vec![event_group.build()])
 }
 
-// Callback to handle deployment result
+/// Callback for handling organization deployment. If deployment was successful, adds the new
+/// organization to state tracking. If unsuccessful, no changes are made to state.
+///
+/// ### Parameters:
+///
+/// * `ctx`: [`ContractContext`], the context of the call.
+/// * `callback_ctx`: [`CallbackContext`], the context of the callback.
+/// * `state`: [`SekivaFactoryState`], the state before the call.
+///
+/// ### Returns:
+/// The new state of type [`SekivaFactoryState`].
 #[callback(shortname = 0x10)]
 fn deploy_organization_callback(
     ctx: ContractContext,
@@ -145,13 +189,22 @@ fn deploy_organization_callback(
         let mut deployed_contracts = state.deployed_contracts.clone();
         deployed_contracts.insert(contract_address, ContractType::Organization {});
 
-        let mut orgs = state.organizations.clone();
-        orgs.insert(contract_address);
+        let mut organizations = state.organizations.clone();
+        organizations.insert(contract_address);
+
+        let mut user_organizations = state.user_organizations.clone();
+        let mut user_org_set = user_organizations
+            .get(&ctx.sender)
+            .cloned()
+            .unwrap_or_default();
+        user_org_set.insert(contract_address);
+        user_organizations.insert(ctx.sender, user_org_set);
 
         return (
             SekivaFactoryState {
-                organizations: orgs,
-                deployed_contracts: deployed_contracts,
+                organizations,
+                user_organizations,
+                deployed_contracts,
                 ..state
             },
             vec![],
@@ -161,7 +214,151 @@ fn deploy_organization_callback(
     (state, vec![])
 }
 
+/// Create a new ballot.
+///
+/// # Arguments
+///
+/// * `ctx` - the contract context containing information about the sender and the blockchain.
+/// * `state` - the current state of the Sekiva Factory.
+/// * `title` - the title of the ballot.
+/// * `description` - the description of the ballot.
+/// * `profile_image` - the profile image of the ballot.
+/// * `banner_image` - the banner image of the ballot.
+///
+/// # Returns
+///
+/// The updated Sekiva Factory state reflecting the new ballot.
+///
+#[action(shortname = 0x01)]
+fn deploy_ballot(
+    ctx: ContractContext,
+    state: SekivaFactoryState,
+    title: String,
+    description: String,
+    options: Vec<String>,
+    organization: Address,
+) -> (SekivaFactoryState, Vec<EventGroup>) {
+    let mut event_group = EventGroup::builder();
+
+    event_group
+        .call(DEPLOY_CONTRACT_ADDRESS, DEPLOY_SHORTNAME)
+        .argument(BINDER_ID)
+        .argument(state.ballot_contract_zkwa.clone())
+        .argument(state.ballot_contract_abi.clone())
+        .argument(create_ballot_init_data(
+            title,
+            description,
+            options,
+            organization,
+            ctx.sender,
+        ))
+        .done();
+
+    event_group
+        .with_callback(SHORTNAME_DEPLOY_BALLOT_CALLBACK)
+        .with_cost(10000)
+        .done();
+
+    (state, vec![event_group.build()])
+}
+
+// Callback to handle organization deployment result
+#[callback(shortname = 0x10)]
+fn deploy_ballot_callback(
+    ctx: ContractContext,
+    callback_ctx: CallbackContext,
+    state: SekivaFactoryState,
+) -> (SekivaFactoryState, Vec<EventGroup>) {
+    if callback_ctx.success {
+        let contract_address = extract_contract_address(&callback_ctx);
+
+        let mut deployed_contracts = state.deployed_contracts.clone();
+        deployed_contracts.insert(contract_address, ContractType::Ballot {});
+
+        let mut ballots = state.ballots.clone();
+        ballots.insert(contract_address);
+
+        let mut user_ballots = state.user_ballots.clone();
+        let mut user_ballot_set = user_ballots.get(&ctx.sender).cloned().unwrap_or_default();
+        user_ballot_set.insert(contract_address);
+        user_ballots.insert(ctx.sender, user_ballot_set);
+
+        return (
+            SekivaFactoryState {
+                ballots,
+                user_ballots,
+                deployed_contracts,
+                ..state
+            },
+            vec![],
+        );
+    }
+
+    (state, vec![])
+}
+
+/// Extract the contract address from the callback context.
+///
+/// # Arguments
+///
+/// * `callback_ctx` - the callback context containing the results of the contract call.
+///
+/// # Returns
 fn extract_contract_address(callback_ctx: &CallbackContext) -> Address {
-    let result = callback_ctx.results.get(0).unwrap();
-    result.get_return_data::<Address>()
+    callback_ctx.results[0].get_return_data::<Address>()
+}
+
+/// Create the initial data for an organization.
+///
+/// # Arguments
+///
+/// * `name` - the name of the organization.
+/// * `description` - the description of the organization.
+/// * `profile_image` - the profile image of the organization.
+/// * `banner_image` - the banner image of the organization.
+///
+/// # Returns
+///
+/// The initial data for an organization.
+fn create_org_init_data(
+    name: String,
+    description: String,
+    profile_image: String,
+    banner_image: String,
+) -> Vec<u8> {
+    let mut bytes = Vec::new();
+    name.rpc_write_to(&mut bytes).unwrap();
+    description.rpc_write_to(&mut bytes).unwrap();
+    profile_image.rpc_write_to(&mut bytes).unwrap();
+    banner_image.rpc_write_to(&mut bytes).unwrap();
+    bytes
+}
+
+/// Create the initial data for a ballot.
+///
+/// # Arguments
+///
+/// * `title` - the title of the ballot.
+/// * `description` - the description of the ballot.
+/// * `options` - the options of the ballot.
+/// * `organization` - the organization of the ballot.
+/// * `administrator` - the administrator of the ballot.
+///
+/// # Returns
+///
+/// The initial data for a ballot.
+fn create_ballot_init_data(
+    title: String,
+    description: String,
+    options: Vec<String>,
+    organization: Address,
+    administrator: Address,
+) -> Vec<u8> {
+    let mut bytes = Vec::new();
+    title.rpc_write_to(&mut bytes).unwrap();
+    description.rpc_write_to(&mut bytes).unwrap();
+    options.rpc_write_to(&mut bytes).unwrap();
+    organization.rpc_write_to(&mut bytes).unwrap();
+    administrator.rpc_write_to(&mut bytes).unwrap();
+    bytes
 }

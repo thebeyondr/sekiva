3d4d2184f899d36a06b41801e50f2f58016db852
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2025-05-07 18:07:08 -0500

    feat: :sparkles: update App layout and styles - Redesigned App component for a cleaner layout with a centered title and button. - Added Google font "Geist" for improved typography. - Introduced new CSS variables for custom colors in index.css.

--- sekiva-frontend/src/App.tsx
+++ sekiva-frontend/src/App.tsx
@@ -29,35 +29,10 @@ function App() {
   };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="text-3xl font-bold underline">Vite + React</h1>
-      <div className="card">
-        <Button onClick={handleConnectWallet}>Connect Wallet</Button>
-
-        {connectionStatus && (
-          <p className="connection-status">{connectionStatus}</p>
-        )}
-
-        {walletAddress && (
-          <p className="wallet-info">Connected to: {walletAddress}</p>
-        )}
-
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="flex flex-col items-center justify-center h-screen bg-sk-yellow-light">
+      <h1 className="text-3xl font-bold">Sekiva</h1>
+      <Button onClick={handleConnectWallet}>Connect Wallet</Button>
+    </div>
   );
 }
 

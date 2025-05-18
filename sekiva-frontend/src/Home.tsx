2ff9655dadd53cf85038c9e81cae25d797c98255
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2025-05-17 20:39:04 -0500

    feat(home): :iphone: update responsive design for home page

--- sekiva-frontend/src/Home.tsx
+++ sekiva-frontend/src/Home.tsx
@@ -9,7 +9,7 @@ function Home() {
     <div className="h-screen bg-sk-yellow-light overflow-auto">
       <div className="container mx-auto max-w-[1500px]">
         <NavBar />
-        <section className="flex flex-col xl:flex-row gap-4 py-10">
+        <section className="flex flex-col-reverse xl:flex-row gap-4 py-10">
           <div className="w-full xl:w-[50%] h-full">
             <img
               src={bauhausIllustration}
@@ -17,11 +17,11 @@ function Home() {
               className="w-full h-full"
             />
           </div>
-          <div className="flex-1 flex flex-col justify-center items-center space-y-3 px-4">
-            <h1 className="text-3xl sm:text-4xl xl:text-6xl font-medium tracking-[-0.04em] max-w-2xl text-center">
+          <div className="flex-1 flex flex-col justify-center items-start md:items-center space-y-3 px-4">
+            <h1 className="text-4xl xl:text-6xl font-medium tracking-[-0.04em] max-w-2xl text-left md:text-center">
               Private on-chain ballots for modern collectives
             </h1>
-            <p className="text-lg max-w-xl text-center">
+            <p className="text-lg max-w-xl text-left md:text-center">
               Build decentralized organizations with secure private voting on{" "}
               <Link
                 to="https://partisiablockchain.com"
@@ -32,11 +32,10 @@ function Home() {
               </Link>
             </p>
 
-            <div className="flex flex-wrap gap-4 justify-center">
+            <div className="flex flex-wrap space-x-2 justify-start md:justify-center">
               <Link to="/collectives">
                 <Button
                   variant="secondary"
-                  size="lg"
                   className="flex items-center gap-2 border-2 border-black"
                 >
                   <LayoutGrid className="w-5 h-5" />
@@ -45,7 +44,7 @@ function Home() {
               </Link>
 
               <Link to="/collectives/new">
-                <Button size="lg" className="flex items-center gap-2">
+                <Button className="flex items-center gap-2">
                   <Plus className="w-5 h-5" />
                   Start Collective
                 </Button>
